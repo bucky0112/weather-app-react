9df60b4c5c557bbd1dc89f74ea13255a89115a56
bucky0112 <bucky0112@gmail.com>
2021-06-12 02:42:44 +0800

    edit style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-// @flow
 import * as React from 'react';
 import styled from '@emotion/styled';
+
+import { ReactComponent as DayCloudyIcon } from './assets/images/day-cloudy.svg';
+import { ReactComponent as AirFlowIcon } from './assets/images/airFlow.svg';
+import { ReactComponent as RainIcon } from './assets/images/rain.svg';
+import { ReactComponent as RefreshIcon } from './assets/images/refresh.svg';
 
 const Container = styled.div`
   background-color: #ededed;
@@ -11,16 +15,107 @@ const Container = styled.div`
 `;
 
 const WeatherCard = styled.div`
+  position: relative;
   min-width: 360px;
   box-shadow: 0 1px 3px 0 #999999;
   background-color: #f9f9f9;
+  box-sizing: border-box;
+  padding: 30px 15px;
+`;
+
+const Location = styled.div`
+  font-size: 28px;
+  color: #212121;
+  margin-bottom: 20px;
+`;
+
+const Description = styled.div`
+  font-size: 16px;
+  color: #828282;
+  margin-bottom: 30px;
+`;
+
+const CurrentWeather = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const Temperature = styled.div`
+  color: #757575;
+  font-size: 96px;
+  font-weight: 300;
+  display: flex;
+`;
+
+const Celsius = styled.div`
+  font-weight: normal;
+  font-size: 42px;
+`;
+
+const AirFlow = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+  margin-bottom: 20px;
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+const Rain = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+const Refresh = styled.div`
+  position: absolute;
+  right: 15px;
+  bottom: 15px;
+  font-size: 12px;
+  display: inline-flex;
+  align-items: flex-end;
+  color: #828282;
+  svg {
+    margin-left: 10px;
+    width: 15px;
+    height: 15px;
+    cursor: pointer;
+  }
+`;
+
+const DayCloudy = styled(DayCloudyIcon)`
+  flex-basis: 30%;
 `;
 
 function App(): React.Node {
   return (
     <Container>
       <WeatherCard>
-        <h1>Weather</h1>
+        <Location>彰化縣</Location>
+        <Description>多雲時晴</Description>
+        <CurrentWeather>
+          <Temperature>
+            23 <Celsius>。C</Celsius>
+          </Temperature>
+          <DayCloudy />
+        </CurrentWeather>
+        <AirFlow><AirFlowIcon />23 m/h</AirFlow>
+        <Rain><RainIcon />48%</Rain>
+        <Refresh>最後觀測時間：上午 12:03<RefreshIcon /></Refresh>
       </WeatherCard>
     </Container>
   );
